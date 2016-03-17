bce77ffc23d858a8f70b041a460f4f92a5b47c66
CemKawa92 <k.cagras@web.de>
2016-03-17 15:53:29 +0100

    created android project with NavigationDrawer-Activity

---
+++ Alarm2000_Android/app/src/androidTest/java/com/alarmcompany/alarm2000_android/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.alarmcompany.alarm2000_android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
